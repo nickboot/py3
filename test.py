b40828a341849b114e74f8fbca5790e2ee18a665
Y50 <y50@Y50.local>
2018-08-12 17:16:51 +0800

    测试上传项目

---
+++ test.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+from selenium import webdriver
+from time import sleep
+browser = webdriver.Chrome()
+url = "http://lpl.qq.com"
+browser.get(url)
+sleep(5)
+print ("退出浏览器")
+browser.quit()
