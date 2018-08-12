c7f2e4c0e2b919557483e52d2cf42cae4aecf622
nickboot <nickbooty50@gmail.com>
2018-08-12 19:11:08 +0800

    设置隐式等待

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from time import sleep
 browser = webdriver.Chrome()
 url = "http://lpl.qq.com"
 browser.get(url)
+browser.implicitly_wait(30)     #隐式等待查找等位元素30秒
 sleep(5)
 print ("退出浏览器")
 browser.quit()
